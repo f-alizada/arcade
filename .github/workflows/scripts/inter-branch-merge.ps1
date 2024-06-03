d88cfe5ceb355b579e32544734c6490cb56a54f5
Farhad Alizada <falizada@microsoft.com>
2024-06-03 14:43:36 +0200

    Share repository name

--- .github/workflows/scripts/inter-branch-merge.ps1
+++ .github/workflows/scripts/inter-branch-merge.ps1
@@ -68,7 +68,6 @@ function Invoke-Block([scriptblock]$cmd) {
 function GetCommitterGitHubName($sha) {
     $email = & git show -s --format='%ce' $sha
     $key = 'committer'
-    Write-Host "Email: $email"
     if ($email -eq '@dotnet-maestro') {
         return 'dotnet-maestro'
     }
@@ -76,13 +75,11 @@ function GetCommitterGitHubName($sha) {
     # Exclude noreply@github.com - these map to https://github.com/web-flow, which is the user account
     # added as the 'committer' when users commit via the GitHub web UI on their own PRs
     if ((-not $email) -or ($email -eq 'noreply@github.com')) {
-        Write-Host "change key to author"
         $key = 'author'
         $email = & git show -s --format='%ae' $sha
     }
 
     if ($email -like '*@users.noreply.github.com') {
-        Write-Host "no-reply detected"
         [string[]] $userNames = ($email -replace '@users.noreply.github.com', '') -split '\+'
         return $userNames | select -last 1
     }
@@ -96,7 +93,7 @@ function GetCommitterGitHubName($sha) {
         try {
             $resp = Invoke-RestMethod -Method GET -Headers $headers `
                 "https://api.github.com/repos/$RepoOwner/$RepoName/commits/$sha"
-            $resp | Write-Host
+            $resp | Write-Verbose
 
             $script:emails[$email] = $resp.$key.login
             return $resp.$key.login
